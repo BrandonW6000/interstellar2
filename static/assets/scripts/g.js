3886f1d64ec27747228818443b17f1ad441fe0e5
BrandonW6000 <147110449+BrandonW6000@users.noreply.github.com>
2024-01-24 01:45:43 +0000

    Update g.js

--- static/assets/scripts/g.js
+++ static/assets/scripts/g.js
@@ -39,14 +39,6 @@ document.addEventListener('DOMContentLoaded', () => {
       error: false,
     },
     {
-      name: 'Geometry Dash',
-      link: 'https://geometrydash-free.com/',
-      image: '/assets/media/icons/geometry-dash.png',
-      categories: ['all'],
-      error: false,
-    },
-    {
-    {
       name: 'Gladihoppers',
       link: 'https://bonbang.github.io/store99/gladihoppers/index.html',
       image: '/assets/media/icons/gladihoppers.jpg',

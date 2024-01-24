df0d2f13ab55f5c5009a9628bb40dd7d205854b7
BrandonW6000 <147110449+BrandonW6000@users.noreply.github.com>
2024-01-24 01:28:00 +0000

    Update apps.js

--- static/assets/scripts/apps.js
+++ static/assets/scripts/apps.js
@@ -1,12 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const appsList = [
  {
-      name: 'interPlanet site',
-      link: 'https://free-4259833.webador.com',
-      image: '/assets/media/icons/apps/gitlogo.png',
-      categories: ['all', 'media'],
-      error: false,
-    },
     {
       name: 'Amazon',
       link: 'https://amazon.com/',
